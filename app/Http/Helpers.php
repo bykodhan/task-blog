8efe92f57d4b060be87a937530082098cf7fac8c
Mustafa Eren Demir <bykodhan@gmail.com>
2023-09-11 13:43:51 +0300

    Update Helpers.php

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -11,10 +11,10 @@ function sendEmail($to, $subject, $text)
         //Server settings
         $mail->SMTPDebug = 0; //Enable verbose debug output
         $mail->isSMTP(); //Send using SMTP
-        $mail->Host = 'email-smtp.us-east-1.amazonaws.com'; //Set the SMTP server to send through
+        $mail->Host = ''; //Set the SMTP server to send through
         $mail->SMTPAuth = true; //Enable SMTP authentication
-        $mail->Username = 'AKIA4KZBJRS475KXFRTR'; //SMTP username
-        $mail->Password = 'BA440w6JzsZi2B+Fx6M+wouofZqe6lsOwWskrV6rO6Ae'; //SMTP password
+        $mail->Username = ''; //SMTP username
+        $mail->Password = ''; //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
         $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
